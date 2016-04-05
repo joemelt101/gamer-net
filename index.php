20627a4f785ded62a97acd2f4f2317b1f6ec5165
Jared Melton <joetemporary@outlook.com>
2016-04-04 20:24:25 -0500

    Fixed some of the addresses

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 //this is the landing page    
-$controllerLocation = '/controller/controllers/landing_controller.php';
+$controllerLocation = 'controller/controllers/landing_controller.php';
 require_once('controller/pagebooter.php');
 ?>
