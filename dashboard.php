1bbd468550fdf14f5bc1a4a649b9ac913acfbcec
Jared Melton <joetemporary@outlook.com>
2016-04-05 23:18:53 -0500

    Removed search.php

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,39 @@
-<?php
-$controllerLocation = '';
-require_once('controller/pagebooter.php');
-?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>This is my page!</title>  
+        <!-- Import Libraries Dynamically so as to change in only one spot... -->
+        <?php require_once('view/views/includes.html'); ?>
+    </head>
+    
+    <body>
+        <?php require_once('view/views/navbar_component.php'); ?>
+        
+        
+        <div class="panel panel-success container-fixed container">
+            <div class="jumbotron">
+                <h1>First Component</h1>
+                <p>This spans the width of the entire page of content! Blah, blah, blah, blah, blah, blah, blah, blah, blah, blah, blah.</p>
+            </div>
+            
+            <div class="row">
+                <div class="col-xs-6">
+                    <h1>Second Component</h1>
+                    <p>This spans the left column only. See how components can be side by side by Bootstrap's row settings?</p>
+                </div>
+                <div class="col-xs-6">
+                        <a href="#" class="thumbnail">
+                            <img src="/gamer-net/view/images/thumbnail.svg">
+                        </a>
+                </div>
+            </div>
+            
+            <div>
+                <h3>Third Component Can Go Here</h3>
+            </div>
+        </div>
+        
+    </body>
+    
+</html>
