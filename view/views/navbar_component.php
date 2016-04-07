4a78d51fb0c3310b2468047d08fda780d4f8b4b7
amstl14 <amstl14@gmail.com>
2016-04-07 06:33:54 +0000

    landing page

--- view/views/navbar_component.php
+++ view/views/navbar_component.php
@@ -17,7 +17,7 @@ $isLoggedIn = true;
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Gamer-Net</a>
+          <a class="navbar-brand" href="index.php">Gamer-Net</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
