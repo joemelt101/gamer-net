aa55121837bedd971736435b216b5994693cb4e5
cmartinezvillar <cmartinezvillar90@gmail.com>
2016-04-06 20:21:05 -0500

    started sprint2 branch

---
+++ view/views/search_page.php
@@ -0,0 +1,21 @@
+<!-- GAMER-NET -- SEARCH PAGE -->
+<html>
+    <head>
+        <title>gamer-net</title> 
+        <link href='stylesheet.css' type='text/css' rel=''></link> 
+        
+        <!-- Import Libraries Dynamically so as to change in only one spot...
+        <?php require_once('/view/views/includes.html'); ?> -->
+    </head>
+    <body>
+        <?php require_once('/view/views/navbar_component.php'); ?>
+        <div class="panel panel-success container-fixed container">
+            <div class="jumbotron"></div>
+            <div class="row">
+                <div class="col-xs-6"></div>
+                <div class="col-xs-6"></div>
+            </div>
+            <div></div>
+        </div>    
+    </body>
+</html>
