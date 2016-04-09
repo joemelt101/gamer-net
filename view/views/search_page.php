e7250601f14b42af086904450d0d0736c7a38cd2
cmartinezvillar <cmartinezvillar90@gmail.com>
2016-04-09 15:47:03 -0500

    working on search page..

--- view/views/search_page.php
+++ view/views/search_page.php
@@ -1,21 +1,50 @@
 <!-- GAMER-NET -- SEARCH PAGE -->
+<!DOCTYPE html>
 <html>
     <head>
-        <title>gamer-net</title> 
-        <link href='stylesheet.css' type='text/css' rel=''></link> 
-        
-        <!-- Import Libraries Dynamically so as to change in only one spot...
-        <?php require_once('/view/views/includes.html'); ?> -->
+        <title>Gamer-net -- Search</title>
+        <!-- Import Libraries Dynamically so as to change in only one spot... -->
+        <?php require_once('view/views/includes.html'); ?>        
+        <style>
+            #searchbar
+            {
+                background-color: crimson
+            }
+            
+            #buttons
+            {
+                background-color: beige;
+            }
+            
+            #friends-panel
+            {
+                background-color: aqua;
+            }
+            
+        </style>
     </head>
-    <body>
-        <?php require_once('/view/views/navbar_component.php'); ?>
-        <div class="panel panel-success container-fixed container">
-            <div class="jumbotron"></div>
-            <div class="row">
-                <div class="col-xs-6"></div>
-                <div class="col-xs-6"></div>
+    <body> 
+        <?php require_once('view/views/navbar_component.php'); ?>
+        
+        <div class='panel fixed-container container'>
+            <div class='panel-body'>
+                <div class='row'>
+                    <div id='searchbar' class='col-xs-6'>
+                        col1
+                    </div>
+                    <div id='buttons' class='col-xs-6'>
+                        col2
+                    </div>
+                </div>
+                <div id='friends-panel' class='row'>
+                    <div class='row'>
+                        friend1
+                    </div>
+                    <div class='row'>
+                        friend2
+                    </div>
+                </div>
             </div>
-            <div></div>
-        </div>    
+        </div>
     </body>
 </html>
